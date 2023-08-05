5f13b097d9d5f05b39e7eac7ddf402c7dc0b3e8a
AnushDubey <anush20.12.20@gmail.com>
2023-08-05 20:58:18 +0530

    log error

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -98,7 +98,7 @@ export async function POST(req: Request, res: Response) {
 
     return NextResponse.json({ gameId: game.id }, { status: 200 });
   } catch (error) {
-    console.error("Error occurred:", error);
+    console.log("Error occurred:", error);
     if (error instanceof z.ZodError) {
       return NextResponse.json(
         { error: error.issues },
